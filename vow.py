338f1676ffee71936f501675adf36c7230c42f66
jchris1901 <jchris1901@gmail.com>
2016-08-11 19:51:30 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,9 +1,8 @@
-n = raw_input()
 def check(a):
     x=['a','e','i','o','u']
     for k in x:
         if ( k == a ):
             return "Vowel"
     return "Consonant"
-
+n = raw_input()
 print check(n)
